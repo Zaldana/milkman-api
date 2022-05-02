60f4fc4c0ac933de330eb1452b93c264e9f2e299
Ruben Zaldanan <rooben.zaldana@gmail.com>
2022-05-02 14:25:52 -0400

    node update

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost.localdomain:3000',
 }));
 app.use(cookieParser())
 app.use(bodyParser.json());
